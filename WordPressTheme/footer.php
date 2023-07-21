3e8083db7ed9a7c60442519622509feebc5988df
hideki watabe <hideki.w.1971@gmail.com>
2023-07-22 06:05:06 +0900

    wordpress mailform作成 2023/7/22 6:05

---
+++ WordPressTheme/footer.php
@@ -0,0 +1,5 @@
+<?php wp_footer(); ?>
+<footer>フッター</footer>
+</body>
+
+</html>
--- WordPressTheme/header.php
+++ WordPressTheme/header.php
@@ -1,14 +1,18 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
-  <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="format-detection" content="telephone=no">
+  <title>ワードプレス（動的サイト） メールフォーム</title>
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/styles.css">
   <?php wp_head(); ?>
 </head>
+
 <body <?php body_class(); ?>>
   <?php wp_body_open(); ?>
   <header class="">
-    <p class="p-test">aaaa</p>
+    <h1><a href="<?php echo home_url(); ?>">ワードプレス（動的サイト） メールフォーム</a></h1>
   </header>
